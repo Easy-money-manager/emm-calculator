512a47ac53264ca0528343f44bf0adfc2aac665e
Evrandil <grzes.zapala@gmail.com>
2026-09-12 10:19:16 +0200

    basic CMakeLists done

---
+++ src/main.cpp
@@ -0,0 +1,36 @@
+#include <raylib.h>
+
+
+int main()
+{
+    InitWindow(800, 500, "EMM Calculator");
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+
+        ClearBackground(BLACK);
+
+        DrawText(
+            "EMM Calculator",
+            40,
+            40,
+            32,
+            RAYWHITE
+        );
+
+        DrawText(
+            "It lives.",
+            40,
+            100,
+            20,
+            GRAY
+        );
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
+}
